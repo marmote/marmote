990847cfc66eccbbb3b89b036a35cb2897d5e92a
Sandor Szilvasi <sszilvasi@users.noreply.github.com>
2011-11-23 23:05:06 +0000

    Backup before implementing sampling frequencies other than 48 kHz.

--- application/PowerBoard/application/UsbAudioDeviceTest/source/usb_desc.c
+++ application/PowerBoard/application/UsbAudioDeviceTest/source/usb_desc.c
@@ -63,7 +63,7 @@ const uint8_t Microphone_ConfigDescriptor[] =
 /*  bNumEndpoints                          */ 0x00u,
 /*  bInterfaceClass                        */ 0x01u,
 /*  bInterfaceSubClass                     */ 0x01u,
-/*  bInterfaceProtocol                     */ 0x0bu,
+/*  bInterfaceProtocol: M_MIC              */ 0x0bu,
 /*  iInterface                             */ 0x00u,
 /*********************************************************************
  AC Header Descriptor
@@ -154,14 +154,16 @@ const uint8_t Microphone_ConfigDescriptor[] =
 /*  bDescriptorSubtype: FORMAT_TYPE        */ 0x02u,
 /*  bFormatType: TYPE_I                    */ 0x01u,
 /*  bNrChannels                            */ 0x01u,
-/*  bSubframeSize                          */ 0x02u,
-/*  bBitResolution                         */ 0x10u,
+/*  bSubframeSize                          */ 0x01u,
+/*  bBitResolution                         */ 0x08u,
+///*  bSubframeSize                          */ 0x02u,
+///*  bBitResolution                         */ 0x10u,
 /*  bSamFreqType                           */ 0x01u,
 ///*  tSamFreq: 96,000 Hz                    */ 0x00u, 0x77u, 0x01u, // !
-///*  tSamFreq: 48,000 Hz                    */ 0x80u, 0xBBu, 0x00u,
+/*  tSamFreq: 48,000 Hz                    */ 0x80u, 0xBBu, 0x00u,
 ///*  tSamFreq: 44,100 Hz                    */ 0x44u, 0xACu, 0x00u,
 ///*  tSamFreq: 24,000 Hz                    */ 0xC0u, 0x5Du, 0x00u,
-/*  tSamFreq: 8,000 Hz                     */ 0x40u, 0x1Fu, 0x00u,
+///*  tSamFreq: 8,000 Hz                     */ 0x40u, 0x1Fu, 0x00u,
 /*********************************************************************
  Endpoint Descriptor (Mono Microphone Standard AS Audio Data EP)
  *********************************************************************/
@@ -169,7 +171,7 @@ const uint8_t Microphone_ConfigDescriptor[] =
 /*  DescriptorType: ENDPOINT               */ 0x05u,
 /*  bEndpointAddress: EP1 IN               */ 0x81u,
 /*  bmAttributes: synchronous              */ 0x0Du,
-/*  wMaxPacketSize: 96 bytes               */ 0x10u, 0x00u, // !
+/*  wMaxPacketSize: 64 bytes               */ 0x40u, 0x00u,
 /*  bInterval: 1 ms                        */ 0x01u,
 /*  bRefresh                               */ 0x00u,
 /*  bSynchAddress                          */ 0x00u,
