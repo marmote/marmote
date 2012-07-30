2d720c198cca64ea3a45f94505e9f53d7a1e17b1
sandor.szilvasi@gmail.com <sandor.szilvasi@gmail.com@1ba9f358-dc1c-8ea8-ff8b-508690f606d3>
2012-07-30 22:39:13 +0000

    Design to test SPI communication between the Yellowstone power supply board (master) and the Teton main board (slave). This setup has issues as the SmartFusion SPI peripheral has fixed DI and DO pint and doesn't change the MISO/MOSI direction based on its master/slave role.

--- trunk/firmware/MainBoard/BoardTest/SoftConsole/BoardTestMss_MSS_CM3_0/BoardTestMss_MSS_CM3_0_app/main.c
+++ trunk/firmware/MainBoard/BoardTest/SoftConsole/BoardTestMss_MSS_CM3_0/BoardTestMss_MSS_CM3_0_app/main.c
@@ -1,4 +1,6 @@
-#include <mss_i2c.h>
+//#include <mss_i2c.h>
+#include <mss_spi.h>
+#include <mss_gpio.h>
 
 #define TETON_I2C_ADDR 0x01u
 #define JOSHUA_I2C_ADDR (0x50u << 1) // 1010 000
@@ -7,8 +9,49 @@
 uint8_t wr_buf[2];
 uint8_t rd_buf[1];
 
+void slave_frame_handler( uint32_t rx_frame )
+{
+	//g_slave_rx_frame = rx_frame;
+	//uint32_t gpio_outputs;
+
+	if ( MSS_GPIO_get_outputs() & MSS_GPIO_0_MASK )
+	{
+		MSS_GPIO_set_output( MSS_GPIO_0, 0 );
+	}
+	else
+	{
+		MSS_GPIO_set_output( MSS_GPIO_0, 1 );
+	}
+}
+
 int main()
 {
+	const uint16_t frame_size = 16u;
+	const uint16_t slave_tx_frame = 0xBABEu;
+	//uint16_t master_rx;
+
+	MSS_GPIO_init( );
+	MSS_GPIO_config( MSS_GPIO_0, MSS_GPIO_OUTPUT_MODE );
+	MSS_GPIO_set_output( MSS_GPIO_0, 1 );
+
+	MSS_SPI_init( &g_mss_spi0 );
+
+	MSS_SPI_configure_slave_mode
+		(
+			&g_mss_spi0,
+			MSS_SPI_MODE0, // TODO: check in datasheet
+			MSS_SPI_PCLK_DIV_2,
+			frame_size
+		);
+
+	MSS_SPI_set_slave_tx_frame( &g_mss_spi0, slave_tx_frame );
+
+	MSS_SPI_set_frame_rx_handler( &g_mss_spi1, slave_frame_handler );
+
+	MSS_SPI_enable( &g_mss_spi0 );
+
+
+	/*
 	mss_i2c_status_t status;
 
 	MSS_I2C_init(
@@ -56,6 +99,7 @@ int main()
 		{
 			while (1);
 		}
+	*/
 
 
 	while( 1 )
