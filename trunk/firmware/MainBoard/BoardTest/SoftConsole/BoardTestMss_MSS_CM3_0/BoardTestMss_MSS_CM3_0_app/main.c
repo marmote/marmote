50cdb4560027a1ed1c0f1614b3282d53ee4e3a55
sandor.szilvasi@gmail.com <sandor.szilvasi@gmail.com@1ba9f358-dc1c-8ea8-ff8b-508690f606d3>
2012-08-01 17:06:01 +0000

    Added support for printing through the SPI port via functions like puts().
    The SPI messages can be received by the Yellowstone power supply board and displayed through the USB console.

--- trunk/firmware/MainBoard/BoardTest/SoftConsole/BoardTestMss_MSS_CM3_0/BoardTestMss_MSS_CM3_0_app/main.c
+++ trunk/firmware/MainBoard/BoardTest/SoftConsole/BoardTestMss_MSS_CM3_0/BoardTestMss_MSS_CM3_0_app/main.c
@@ -1,6 +1,6 @@
-//#include <mss_i2c.h>
 #include <mss_spi.h>
 #include <mss_gpio.h>
+#include <stdio.h>
 
 #define TETON_I2C_ADDR 0x01u
 #define JOSHUA_I2C_ADDR (0x50u << 1) // 1010 000
@@ -9,11 +9,9 @@
 uint8_t wr_buf[2];
 uint8_t rd_buf[1];
 
-void slave_frame_handler( uint32_t rx_frame )
-{
-	//g_slave_rx_frame = rx_frame;
-	//uint32_t gpio_outputs;
 
+void GPIO1_IRQHandler( void )
+{
 	if ( MSS_GPIO_get_outputs() & MSS_GPIO_0_MASK )
 	{
 		MSS_GPIO_set_output( MSS_GPIO_0, 0 );
@@ -22,87 +20,59 @@ void slave_frame_handler( uint32_t rx_frame )
 	{
 		MSS_GPIO_set_output( MSS_GPIO_0, 1 );
 	}
+	MSS_GPIO_clear_irq( MSS_GPIO_1 );
 }
 
 int main()
 {
-	const uint16_t frame_size = 16u;
-	const uint16_t slave_tx_frame = 0xBABEu;
-	//uint16_t master_rx;
+	//const uint8_t frame_size = 8;
+	//const uint8_t master_tx_frame = 0xDEu;
+	uint8_t ctr = 0;
+	uint8_t tx_frame = ',';
 
+	// Set up GPIOs
 	MSS_GPIO_init( );
 	MSS_GPIO_config( MSS_GPIO_0, MSS_GPIO_OUTPUT_MODE );
-	MSS_GPIO_set_output( MSS_GPIO_0, 1 );
+	MSS_GPIO_set_output( MSS_GPIO_0, 0 );
 
-	MSS_SPI_init( &g_mss_spi0 );
+	MSS_GPIO_config ( MSS_GPIO_1, MSS_GPIO_INPUT_MODE | MSS_GPIO_IRQ_EDGE_POSITIVE );
+	//MSS_GPIO_config ( MSS_GPIO_1, MSS_GPIO_INPUT_MODE | MSS_GPIO_IRQ_LEVEL_HIGH );
+	MSS_GPIO_enable_irq( MSS_GPIO_1 );
+	NVIC_EnableIRQ( GPIO1_IRQn );
 
-	MSS_SPI_configure_slave_mode
-		(
-			&g_mss_spi0,
-			MSS_SPI_MODE0, // TODO: check in datasheet
-			MSS_SPI_PCLK_DIV_2,
-			frame_size
-		);
-
-	MSS_SPI_set_slave_tx_frame( &g_mss_spi0, slave_tx_frame );
-
-	MSS_SPI_set_frame_rx_handler( &g_mss_spi1, slave_frame_handler );
-
-	MSS_SPI_enable( &g_mss_spi0 );
-
+	// Initialize SPI master
 
 	/*
-	mss_i2c_status_t status;
-
-	MSS_I2C_init(
-		&g_mss_i2c0,
-		TETON_I2C_ADDR,
-		MSS_I2C_PCLK_DIV_256
-		);
-
-	wr_buf[0] = 0; // ADDR
-	wr_buf[1] = 0xF4; // DATA
-
-	rd_buf[0] = 0;
-
-	MSS_I2C_write(
-		&g_mss_i2c0,
-		JOSHUA_I2C_ADDR,
-		wr_buf,
-		2,
-		MSS_I2C_RELEASE_BUS
-		);
-
-	status = MSS_I2C_wait_complete( &g_mss_i2c0 );
-
-	if ( status != MSS_I2C_SUCCESS )
-	{
-		while (1);
-	}
-
-	int i;
-	for (i = 0; i < 1000000; i++);
-
-	MSS_I2C_write_read(
-		&g_mss_i2c0,
-		JOSHUA_I2C_ADDR,
-		wr_buf, // addr
-		1, // length of addr
-		rd_buf,
-		1,
-		MSS_I2C_RELEASE_BUS
-		);
-
-	status = MSS_I2C_wait_complete( &g_mss_i2c0 );
-
-		if ( status != MSS_I2C_SUCCESS )
-		{
-			while (1);
-		}
+	MSS_SPI_init( &g_mss_spi0 );
+	MSS_SPI_configure_master_mode
+	(
+		&g_mss_spi0,
+		MSS_SPI_SLAVE_0,
+		MSS_SPI_MODE0,
+		MSS_SPI_PCLK_DIV_128,
+		frame_size
+	);
+	MSS_SPI_enable( &g_mss_spi0 );
 	*/
 
-
+	char obuf[32];
 	while( 1 )
 	{
+		// Periodically send a character
+		int i;
+		for (i = 0; i < 100000; i++);
+
+		tx_frame = (uint8_t)'0' + ctr;
+
+		/*
+		MSS_SPI_set_slave_select( &g_mss_spi0, MSS_SPI_SLAVE_0 );
+		MSS_SPI_transfer_frame( &g_mss_spi0, tx_frame );
+		MSS_SPI_clear_slave_select( &g_mss_spi0, MSS_SPI_SLAVE_0 );
+		*/
+
+		sprintf(obuf, "Hejje-hujja N: %c", tx_frame);
+		puts(obuf);
+
+		ctr = (ctr + 1) % 10;
 	}
 }
