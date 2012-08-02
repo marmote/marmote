3db3bf1ed9cea55ddcd94c2deed37b832742010b
sandor.szilvasi@gmail.com <sandor.szilvasi@gmail.com@1ba9f358-dc1c-8ea8-ff8b-508690f606d3>
2012-08-02 20:33:09 +0000

    Added command processor for SPI ASCII commands.

--- trunk/firmware/MainBoard/BoardTest/SoftConsole/BoardTestMss_MSS_CM3_0/BoardTestMss_MSS_CM3_0_app/main.c
+++ trunk/firmware/MainBoard/BoardTest/SoftConsole/BoardTestMss_MSS_CM3_0/BoardTestMss_MSS_CM3_0_app/main.c
@@ -1,109 +1,79 @@
-#include <mss_spi.h>
-#include <mss_gpio.h>
-#include <stdio.h>
 
-#define TETON_I2C_ADDR 0x01u
-#define JOSHUA_I2C_ADDR (0x50u << 1) // 1010 000
-//#define JOSHUA_I2C_ADDR 0xF0u
+#include <string.h>
+#include "yellowstone.h"
+#include "cmd_def.h"
 
-enum { spi_rx_buffer_size = 8 };
-uint8_t spi_rx_buffer[spi_rx_buffer_size];
-uint8_t spi_rx_buffer_start = 0;
-uint8_t spi_rx_buffer_count = 0;
-uint8_t rx_data;
-
-uint8_t cmd_buffer[spi_rx_buffer_size];
-uint8_t cmd_done;
-
-void GPIO1_IRQHandler( void )
-{
-	MSS_GPIO_set_output( MSS_GPIO_0, 1 );
-
-	MSS_SPI_set_slave_select( &g_mss_spi0, MSS_SPI_SLAVE_0 );
-	rx_data = MSS_SPI_transfer_frame( &g_mss_spi0, '.' );
-	MSS_SPI_clear_slave_select( &g_mss_spi0, MSS_SPI_SLAVE_0 );
-
-	uint8_t end = (spi_rx_buffer_start + spi_rx_buffer_count) % spi_rx_buffer_size;
-	spi_rx_buffer[end] = rx_data;
-
-	if ( spi_rx_buffer_count != spi_rx_buffer_size )
-	{
-		spi_rx_buffer_count++;
-	}
-
-	cmd_done = 0;
-	if ( spi_rx_buffer[end] == '!' )
-	{
-		uint8_t i;
-		for ( i = 0; i < spi_rx_buffer_count; i++ )
-		{
-			cmd_buffer[i] = spi_rx_buffer[spi_rx_buffer_start + i];
-		}
-		cmd_done = 1;
-	}
-	/*
-	MSS_SPI_set_slave_select( &g_mss_spi0, MSS_SPI_SLAVE_0 );
-	MSS_SPI_transfer_frame( &g_mss_spi0, rx_data+1 );
-	MSS_SPI_clear_slave_select( &g_mss_spi0, MSS_SPI_SLAVE_0 );
-
-	MSS_SPI_set_slave_select( &g_mss_spi0, MSS_SPI_SLAVE_0 );
-	MSS_SPI_transfer_frame( &g_mss_spi0, '\n' );
-	MSS_SPI_clear_slave_select( &g_mss_spi0, MSS_SPI_SLAVE_0 );
-	*/
-
-	//puts( (const char*)spi_rx_buffer );
-
-	MSS_GPIO_set_output( MSS_GPIO_0, 0 );
-	MSS_GPIO_clear_irq( MSS_GPIO_1 );
-}
+extern uint8_t cmd_buffer[];
+extern uint8_t cmd_length;
 
 
+char* cmd_token;
+CMD_Type* cmd_list_ptr;
+char* arg_list[10];
+uint32_t argc;
+
+void process_cmd_buff(const char* cmd_buff, uint8_t length);
 
 
 int main()
 {
-	const uint8_t frame_size = 8;
-	//const uint8_t master_tx_frame = 0xDEu;
-	uint8_t ctr = 0;
-	uint8_t tx_frame = ',';	// Set up GPIOs
-	MSS_GPIO_init( );
-	MSS_GPIO_config( MSS_GPIO_0, MSS_GPIO_OUTPUT_MODE );
-	MSS_GPIO_set_output( MSS_GPIO_0, 0 );
+	Yellowstone_Init();
 
-	// Initialize SPI master
-	MSS_SPI_init( &g_mss_spi0 );
-	MSS_SPI_configure_master_mode
-	(
-		&g_mss_spi0,
-		MSS_SPI_SLAVE_0,
-		MSS_SPI_MODE0,
-		MSS_SPI_PCLK_DIV_128,
-		frame_size
-	);
-	MSS_SPI_enable( &g_mss_spi0 );
-
-	MSS_GPIO_config ( MSS_GPIO_1, MSS_GPIO_INPUT_MODE | MSS_GPIO_IRQ_LEVEL_HIGH );
-	MSS_GPIO_enable_irq( MSS_GPIO_1 );
-	NVIC_EnableIRQ( GPIO1_IRQn );;;;
-
-	char obuf[32];
 	while( 1 )
 	{
-		// Periodically send a character
-		int i;
-		for (i = 0; i < 1000000; i++);
-
-		tx_frame = (uint8_t)'0' + ctr;
-
-		/*
-		MSS_SPI_set_slave_select( &g_mss_spi0, MSS_SPI_SLAVE_0 );
-		MSS_SPI_transfer_frame( &g_mss_spi0, tx_frame );
-		MSS_SPI_clear_slave_select( &g_mss_spi0, MSS_SPI_SLAVE_0 );
-		*/
-
-		sprintf(obuf, "N: %c", tx_frame);
-		puts(obuf);
-
-		ctr = (ctr + 1) % 10;
+		if (cmd_length > 0)
+		{
+			process_cmd_buff((const char*)cmd_buffer, cmd_length);
+		}
 	}
+}
+
+
+
+void process_cmd_buff(const char* cmd_buff, uint8_t length)
+{
+	uint8_t parse_result = 1;
+
+	// Tokenize RX buffer content
+	cmd_token = strtok((char *)cmd_buffer, " ");
+	if (cmd_token != NULL)
+	{
+		cmd_list_ptr = CMD_List;
+		while (cmd_list_ptr->CmdString)
+	    {
+	        if (!strcmp(cmd_token, (const char*)cmd_list_ptr->CmdString))
+	        {
+				// Set the argList pointers to tokens
+	            argc = 0;
+
+	            while ( cmd_token != NULL && argc < 10 )
+	            {
+	                arg_list[argc++] = cmd_token;
+	                cmd_token = strtok(NULL, " ");
+	            }
+
+				Yellowstone_write("\nACK", 4);
+
+				// Invoke associated command function
+		        cmd_list_ptr->CmdFunction(argc, arg_list);
+
+				parse_result = 0;
+	            break;
+	        }
+
+			cmd_list_ptr++;
+	    }
+	}
+
+	// Send NAK if no valid command identified
+	if (parse_result == 1)
+	{
+		Yellowstone_write("\nNAK>", 4);
+	}
+
+	// Send '>' prompt
+	Yellowstone_write("\n>", 2);
+
+	// TODO: Clean up states
+	cmd_length = 0;
 }
---
+++ trunk/firmware/MainBoard/BoardTest/SoftConsole/BoardTestMss_MSS_CM3_0/BoardTestMss_MSS_CM3_0_app/yellowstone.c
@@ -0,0 +1,86 @@
+
+#include "yellowstone.h"
+
+enum { spi_frame_size = 8 };
+enum { spi_rx_buffer_size = 8 };
+
+uint8_t spi_rx_buffer[spi_rx_buffer_size];
+uint8_t spi_rx_buffer_start = 0;
+uint8_t spi_rx_buffer_count = 0;
+uint8_t rx_data;
+
+uint8_t cmd_buffer[spi_rx_buffer_size];
+
+
+void Yellowstone_Init(void)
+{
+	MSS_GPIO_init( );
+	MSS_GPIO_config( MSS_GPIO_0, MSS_GPIO_OUTPUT_MODE );
+	MSS_GPIO_set_output( MSS_GPIO_0, 0 );
+
+	// Initialize SPI master
+	MSS_SPI_init( &g_mss_spi0 );
+	MSS_SPI_configure_master_mode
+	(
+		&g_mss_spi0,
+		MSS_SPI_SLAVE_0,
+		MSS_SPI_MODE0,
+		MSS_SPI_PCLK_DIV_128,
+		spi_frame_size
+	);
+	MSS_SPI_enable( &g_mss_spi0 );
+
+	MSS_GPIO_config ( MSS_GPIO_1, MSS_GPIO_INPUT_MODE | MSS_GPIO_IRQ_LEVEL_HIGH );
+	MSS_GPIO_enable_irq( MSS_GPIO_1 );
+	NVIC_EnableIRQ( GPIO1_IRQn );
+}
+
+void Yellowstone_write( const char* data, uint8_t length )
+{
+	uint8_t i;
+
+	for ( i = 0; i < length; i++ )
+	{
+		MSS_SPI_set_slave_select( &g_mss_spi0, MSS_SPI_SLAVE_0 );
+		rx_data = MSS_SPI_transfer_frame( &g_mss_spi0, *(data+i) );
+		MSS_SPI_clear_slave_select( &g_mss_spi0, MSS_SPI_SLAVE_0 );
+	}
+}
+
+void Yellowstone_print( const char* data )
+{
+	unsigned int length = strlen(data);
+	Yellowstone_write( data, length );
+	Yellowstone_write( '\n', 1 );
+}
+
+void GPIO1_IRQHandler( void )
+{
+	MSS_SPI_set_slave_select( &g_mss_spi0, MSS_SPI_SLAVE_0 );
+	rx_data = MSS_SPI_transfer_frame( &g_mss_spi0, '.' );
+	MSS_SPI_clear_slave_select( &g_mss_spi0, MSS_SPI_SLAVE_0 );
+
+	uint8_t end = (spi_rx_buffer_start + spi_rx_buffer_count) % spi_rx_buffer_size;
+	spi_rx_buffer[end] = rx_data;
+
+	if ( spi_rx_buffer_count != spi_rx_buffer_size )
+	{
+		spi_rx_buffer_count++;
+	}
+
+	cmd_length = 0;
+	if ( spi_rx_buffer[end] == '\n' )
+	{
+		uint8_t i;
+		for ( i = 0; i < spi_rx_buffer_count-1; i++ )
+		{
+			cmd_buffer[i] = spi_rx_buffer[spi_rx_buffer_start + i];
+		}
+		cmd_length = spi_rx_buffer_count-1;
+
+		spi_rx_buffer_start = cmd_length % spi_rx_buffer_size;
+		spi_rx_buffer_count = 0;
+	}
+
+	MSS_GPIO_clear_irq( MSS_GPIO_1 );
+}
