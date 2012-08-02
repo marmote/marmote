0e253d33713b91d5f0cbf3093ea08811bee181f0
sandor.szilvasi@gmail.com <sandor.szilvasi@gmail.com@1ba9f358-dc1c-8ea8-ff8b-508690f606d3>
2012-08-02 22:42:47 +0000

    Added support for parametrized commands.

--- trunk/firmware/PowerBoard/application/UsbCdcDeviceTest/source/cmd_def.c
+++ trunk/firmware/PowerBoard/application/UsbCdcDeviceTest/source/cmd_def.c
@@ -1,36 +1,5 @@
 #include "cmd_def.h"
 
-/*
-uint8_t LedOn(void)
-{
-	LED_On(LED2);
-	return 0;
-}
-
-uint8_t LedOff(void)
-{
-	LED_Off(LED2);
-	return 0;
-}
-
-uint8_t PrintHelpMsg(void)
-{
-    USB_SendMsg("\nhelp mess\n", 11);
-    return 0;
-}
-
-uint8_t CMD_PowerOn(void)
-{
-	LED_On(LED2);
-	return 0;
-}
-
-uint8_t CMD_PowerOff(void)
-{
-	LED_Off(LED2);
-	return 0;
-}
-*/
 
 // List of command words and associated functions
 extern CMD_Type CMD_List[] =
@@ -40,7 +9,6 @@ extern CMD_Type CMD_List[] =
 	"pwr",  CmdPwr,
 	"reg",  CmdReg,
 	"t",    CmdTeton,
-//	"ccreg",  CmdCcReg,
 //	"mon on", CMD_MonitorOn,
 //	"mon off", CMD_MonitorOff
 	NULL,   NULL
@@ -166,110 +134,29 @@ uint32_t CmdReg(uint32_t argc, char** argv)
 
 uint32_t CmdTeton(uint32_t argc, char** argv)
 {	
-	if (argc == 2) {
-		//if ( strlen(*(argv+1)) == 1 )		
-		{
-			char buf[32];
-			uint8_t arg_len;
-			
-			arg_len = strlen(*(argv+1)) < 32-1 ? strlen(*(argv+1)) : 32-1;
-			strncpy( buf, *(argv+1), arg_len );
-			buf[arg_len] = '\n';
+	char buf[128];
+	uint8_t len;
+	uint8_t i;
 
-			CON_SPI_Write( (const uint8_t*)buf, arg_len+1 );
-			return 0;
+	if ( argc > 1 )
+	{
+		// FIXME: no length check
+		strcpy(buf, *(argv+1));
+		strcat(buf, " ");
+		for ( i = 2 ; i < argc ; i++ )
+		{
+			strcat(buf, *(argv+i));
+			strcat(buf, " ");
 		}
-		
-		/*
-		if (!strcmp(*(argv+1), "on"))
-		{			
-			LED_On(LED2);
-			CON_SGPIO3_GPIO_PORT->BSRR = CON_SGPIO3_PIN;
-			CON_SGPIO2_GPIO_PORT->BRR = CON_SGPIO2_PIN;
-			USB_SendString("\nSF IRQ line asserted");
-			return 0;
-		}
-	
-		if (!strcmp(*(argv+1), "off"))
-		{			
-			LED_Off(LED2);
-			CON_SGPIO3_GPIO_PORT->BRR = CON_SGPIO3_PIN;
-			CON_SGPIO2_GPIO_PORT->BSRR = CON_SGPIO2_PIN;
-			USB_SendString("\nSF IRQ line deasserted");
-			return 0;
-		}
-		*/
+		// Replace last space with '\n'
+		len = strlen(buf);
+		buf[len-1] = '\n';
+
+		CON_SPI_Write( (const uint8_t*)buf, len+1 );
+		return 0;
 	}
 
 	// Send help message
   	USB_SendString("\nUsage: t <msg to forward>");
 	return 1;
 }
-
-
-//uint32_t CmdCcReg(uint32_t argc, char** argv)
-//{		
-//	uint8_t addr, data;
-//	char msg[32];
-//
-//	if (argc == 2 || argc == 3)
-//	{
-//		// Parse first paramter (check if it's a valid number)
-//		addr = atoi(*(argv+1));
-//
-//		if (addr || !strcmp(*(argv+1), "0"))
-//		{
-//			if (argc == 2)
-//			{
-//				data = CON_SPI_ReadRegister(addr);
-//				USB_SendString("\nreg read");
-//				// Print register value
-//				//itoa(data, msg, 10);
-//				sprintf(msg, "\n%3d: %3d", addr, data);
-//				//USB_SendString("\n");
-//				USB_SendString(msg);
-//				return 0;
-//			}
-//
-//			// Parse second parameter
-//			data = atoi(*(argv+2));
-//			if (data || !strcmp(*(argv+2), "0"))
-//			{
-//				CON_SPI_WriteRegister(addr, data);
-//				USB_SendString("\nreg written");
-//				sprintf(msg, "\n%3d: %3d (w)", addr, data);
-//				USB_SendString(msg);
-//				
-//				// Read back register value here
-//				data = CON_SPI_ReadRegister(addr);
-//				// Print new register value		  
-//				sprintf(msg, "\n%3d: %3d (r)", addr, data);
-//				USB_SendString(msg);
-//				//itoa(data, msg, 10);
-//				return 0;
-//			}
-//		}
-//	}
-//			
-//	// Send help message
-//	USB_SendString("\nUsage: reg <addr> [<new value>]");
-//	return 1;	
-//}
-				   /*
-uint8_t PrintHelpMsg(void)
-{
-	uint8_t cmdIndex;
-
-    USB_SendString("\nAvailable commands:\n\n");
-
-	for (cmdIndex = 0; cmdIndex < sizeof(CMD_List)/sizeof(CMD_Type); cmdIndex++)
-	{
-		USB_SendString((char*)CMD_List[cmdIndex].CmdString);
-		USB_SendString("\n");
-	}
-
-    return 0;
-}
-*/
-
-
