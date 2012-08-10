92fdad3d51367011d0467615ef1ae9b6764403e0
Sandor Szilvasi <sszilvasi@users.noreply.github.com>
2012-08-10 18:02:07 +0000

    Added an 'env' command to be able to switch between the Yellowstone and Teton boards on the console.

--- firmware/PowerBoard/application/UsbCdcDeviceTest/include/cmd_def.h
+++ firmware/PowerBoard/application/UsbCdcDeviceTest/include/cmd_def.h
@@ -9,19 +9,37 @@
 #include "power_board.h"
 #include "teton.h"
 
- #include "usb_fs.h"
+#include "usb_fs.h"
 
+typedef enum _ENV_Enum_Type
+{
+	ENV_NONE	 	= 0,
+	ENV_ALL	 		= 1,
+	ENV_Yellowstone	= 2,
+	ENV_Teton		= 4
+} ENV_Enum_Type;
 
 typedef struct _CMD_Type
 {
-	uint8_t* CmdString;
+	const char* CmdString;
 	uint32_t (*CmdFunction)(uint32_t argc, char** argv);
+	uint32_t CmdEnv; // Environments the command available in
 } CMD_Type;
 
+typedef struct _ENV_Type
+{
+	const char* envString;
+	ENV_Enum_Type envEnum;
+} ENV_Type;
 
+				   
 extern CMD_Type CMD_List[];
+extern ENV_Type ENV_List[];
+
+extern ENV_Enum_Type activeEnv;
 
 uint32_t CmdHelp(uint32_t argc, char** argv);
+uint32_t CmdEnv(uint32_t argc, char** argv);
 uint32_t CmdLed(uint32_t argc, char** argv);
 uint32_t CmdPwr(uint32_t argc, char** argv);
 uint32_t CmdReg(uint32_t argc, char** argv);
