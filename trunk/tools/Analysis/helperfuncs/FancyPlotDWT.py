01b9cf9e5ec39fc4c1df48400fd48e00f412e860
babjak.b@gmail.com <babjak.b@gmail.com@1ba9f358-dc1c-8ea8-ff8b-508690f606d3>
2012-10-23 20:59:49 +0000

    - Wavelet tr only working for small sample numbers - Created Visual Studio project for helper functions

--- trunk/tools/Analysis/helperfuncs/FancyPlotDWT.py
+++ trunk/tools/Analysis/helperfuncs/FancyPlotDWT.py
@@ -1,14 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def func1(N, c, Fs) :
-	Fs = float(Fs)
-	f = [0]
-	for ii in xrange(len(c)) :
-		f.append(f[-1] + Fs * len(c[ii]) / N)
 
-	t = np.arange(N+1)/Fs
-	return f, t
+def func1(c, Fs) :
+    N = 0
+    for ii in xrange(len(c)) :
+        N += c[ii].size
+
+    Fs = float(Fs)
+    t = np.arange(N+1)/Fs
+
+    Fs=Fs/2
+    f = [0]
+    for ii in xrange(len(c)) :
+        f.append(f[-1] + Fs * len(c[ii]) / N)
+
+    return f, t
 
 
 def func(f, t, c) :
@@ -24,13 +31,13 @@ def func(f, t, c) :
 		for column in xrange(columns) :
 			index = column * len(c[row]) / (len(t)-1)
 			res[row, column] = np.abs(c[row][index])
-            
+
 	return X, Y, res
 
 	
-def FancyPlotDWT(N, c, Fs) :			
+def FancyPlotDWT(c, Fs) :			
 	Fs = float(Fs)
-	f, t = func1(N, c, Fs)
+	f, t = func1(c, Fs)
 	X, Y, Z = func(f, t, c)
 
 	plt.figure()
@@ -38,3 +45,36 @@ def FancyPlotDWT(N, c, Fs) :
 	plt.colorbar()
 	plt.axis([t[0], t[-1], f[0], f[-1]])
 	plt.show()
+
+
+
+################################################################################
+if __name__ == "__main__":
+
+    c = [ np.array([1, 0]),
+    np.array([0, 1]),
+    np.array([1, 0, 1, 0]),
+    np.array([0, 1, 0, 0.9, 0, 1, 0, 1]),
+    np.array([1, 0, 1, 0, 1, 0, 1, 0]),
+    np.array([1, 0, 0.5, 0]),
+    np.array([1, 0]),
+    np.array([0, 1]), 
+    np.array([0, 1]),
+    np.array([1, 0, 1, 0]),
+    np.array([0, 1, 0, 0.9, 0, 1, 0, 1]),
+    np.array([1, 0, 1, 0, 1, 0, 1, 0]),
+    np.array([1, 0, 0.5, 0, 1, 0, 0.3, 1, 0.6, 0, 0.1, 1, 1, 0, 1, 0]),
+    np.array([0, 1, 0, 0.9, 0, 1, 0, 1]),
+    np.array([1, 0, 1, 0, 1, 0, 1, 0]),
+    np.array([1, 0, 0.5, 0]),
+    np.array([1, 0]),
+    np.array([0, 1]), 
+    np.array([0, 1]),
+    np.array([1, 0, 1, 0]),
+    np.array([0, 1, 0, 0.9, 0, 1, 0, 1]),
+    np.array([1, 0, 1, 0, 1, 0, 1, 0]),
+    np.array([1, 0, 0.5, 0]),
+    np.array([1, 0]),
+    np.array([0, 1]) ]
+
+    FancyPlotDWT(c, 1)
