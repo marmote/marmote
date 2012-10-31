e101f7d56a91c1bd4349eb47b7e5a283e5180fe9
babjak.b@gmail.com <babjak.b@gmail.com@1ba9f358-dc1c-8ea8-ff8b-508690f606d3>
2012-10-31 21:24:34 +0000

    - Support added for more complex wavelet functions

--- trunk/tools/Analysis/helperfuncs/CompleteDWT.py
+++ trunk/tools/Analysis/helperfuncs/CompleteDWT.py
@@ -1,29 +1,35 @@
 import numpy as np
 import pywt
 
-def DWT_recursive(c, flip = False) :
+def DWT_recursive(c, N=None, flip = False, wavelet='haar') :
     #init
     cll = []
     cl = []
     
+    if N is None:
+        N = c.size
+
+    start_idx = int((c.size - N)/2)
+    stop_idx = start_idx + N
+
     c = c.astype(np.float64)
-    cl.append(c)
+    cl.append(c[start_idx:stop_idx])
     cll.append(cl)
     
     #exit condition
-    if c.size <= 1 or c.size % 2 :
+    if N <= 1 or N % 2 :
         return cll 
     
     #actual data generation for this level
-    w = pywt.Wavelet('haar')
+    w = pywt.Wavelet(wavelet)
     if flip :
         cHigh, cLow = pywt.dwt(c, w)
     else :
         cLow, cHigh = pywt.dwt(c, w)
         
     #recursive levels
-    cll_Low = DWT_recursive(cLow)
-    cll_High = DWT_recursive(cHigh, flip = True)
+    cll_Low = DWT_recursive(cLow, N=N/2, wavelet=wavelet)
+    cll_High = DWT_recursive(cHigh, N=N/2, flip = True, wavelet=wavelet)
     
     for ii in xrange(len(cll_Low)) :
         cll.append(cll_Low[ii] + cll_High[ii])
@@ -31,5 +37,5 @@ def DWT_recursive(c, flip = False) :
     return cll
         
         
-def CompleteDWT(x) :		
-	return DWT_recursive(x)
+def CompleteDWT(x, wavelet='haar') :		
+	return DWT_recursive(x, wavelet=wavelet)
