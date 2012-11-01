d038cd6a417d70cdacd9227633af78d94c0b6683
babjak.b@gmail.com <babjak.b@gmail.com@1ba9f358-dc1c-8ea8-ff8b-508690f606d3>
2012-11-01 21:15:08 +0000

    - proper support for different Wavelet functions

--- trunk/tools/Analysis/helperfuncs/UsablePacketTableLevels.py
+++ trunk/tools/Analysis/helperfuncs/UsablePacketTableLevels.py
@@ -1,24 +1,27 @@
-def GetUsefulDWTlevels(min_div, cll) :
+import numpy as np
+
+
+def UsablePacketTableLevels(min_div, PacketTable, N) :
     start_level = 0
-    while start_level < xrange(len(cll)) :
-        if len(cll[start_level]) >= min_div :
+    while start_level < xrange(len(PacketTable)) :
+        if len(PacketTable[start_level]) >= min_div :
             break; 
         start_level += 1
 
-    stop_level = len(cll)-1
+
+    stop_level = len(PacketTable)-1
     while stop_level >= 0 :
-        if cll[stop_level][0].size >= min_div :
+        if N / len(PacketTable[stop_level]) >= min_div :
             break; 
         stop_level -= 1
+
 
     if start_level > stop_level :
         start_level = None
         stop_level = None
 
+
     return start_level, stop_level
-
-    
-
 
 
 ################################################################################
