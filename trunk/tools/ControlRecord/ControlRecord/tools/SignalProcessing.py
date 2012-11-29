- FFT calculation corrected - Frame collection with FIR HPF - File source rewritten - File sink rewritten, it opens a new file for every threshold event - New Threshold block added

trunk/tools/ControlRecord/ControlRecord/tools/SignalProcessing.py
@@ -62,14 +62,14 @@ class SignalProcessingChain :
             I_buff = self.I_HPF.Process(I_buff)
             Q_buff = self.Q_HPF.Process(Q_buff)
 
-        I_spectrum = 2*abs(np.fft.rfft(I_buff))
-        Q_spectrum = 2*abs(np.fft.rfft(Q_buff))
+        I_spectrum = abs(np.fft.rfft(I_buff))
+        Q_spectrum = abs(np.fft.rfft(Q_buff))
     
-        I_spectrum = I_spectrum / N
-        Q_spectrum = Q_spectrum / N
+#        I_spectrum = I_spectrum / N
+#        Q_spectrum = Q_spectrum / N
 
-        I_spectrum = 20 * np.log10(I_spectrum)
-        Q_spectrum = 20 * np.log10(Q_spectrum)
+        I_spectrum = 20 * np.log10(I_spectrum) - 10 * np.log10(N)
+        Q_spectrum = 20 * np.log10(Q_spectrum) - 10 * np.log10(N)
 
         if RF:
             num_pos_fr  = int(DSPconf.num_pos_fr(N))

trunk/tools/ControlRecord/ControlRecord_GNUradio/GNUradio_blocks/FrameToFileSink.py
@@ -28,11 +28,12 @@ class FrameToFileSink(gr.block):
 
 ################################################################################
     def __init__(self):
-        gr.block.__init__(self, name="Generic Data Source Two channel 16 bit data source", in_sig=[np.int16], out_sig=None)
+        gr.block.__init__(self, name="File sink for framed data", in_sig=[np.int16], out_sig=None)
 
         self.FrameConf = FC.Frameconf_t()
 
-        self.f = open('./collect_%d.bin'%1234, 'wb')
+        self.file_counter = 0
+        self.f = None
 
 
 ################################################################################
@@ -42,9 +43,15 @@ class FrameToFileSink(gr.block):
 
 ################################################################################
     def work(self, input_items, output_items):
+#        g = open('./work.txt', 'ab')
+#        g.write("FrameToFileSink start\n")
+#        g.close
+
         nread = self.nitems_read(0) #number of items read on port 0
         ninput_items = len(input_items[0])
-        input = np.array(input_items[0], dtype=np.uint16)
+
+        input = np.array(input_items[0], dtype=np.int16)
+        input.byteswap(True)
 
         #read all tags associated with port 0 for items in this work function
         tags = self.get_tags_in_range(0, nread, nread+ninput_items)
@@ -52,41 +59,97 @@ class FrameToFileSink(gr.block):
 
         frame_starts = []
         frame_cnt = np.array([], dtype=np.uint32)
+        threshold_events = []
+
         for tag in tags:
             key_string = pmt.pmt_symbol_to_string(tag.key)
             if key_string == "Frame counter" :
-                frame_starts.append( tag.offset - nread )
+                frame_start = tag.offset - nread
+
+                if frame_start in frame_starts :
+                    continue
+            
+                frame_starts.append( frame_start )
                 frame_cnt = np.append(frame_cnt, np.uint32(int(pmt.pmt_symbol_to_string(tag.value))) )
+
+            elif key_string == "Threshold event" :
+                threshold_event = tag.offset - nread
+
+                if threshold_event in threshold_events :
+                    continue
+            
+                threshold_events.append( threshold_event )
+                
+
+
+#        f = open('./frame_temp.txt', 'ab')
+
+#        f.write('BEGINBEGINBEGIN\n')
+
+#        f.write('Threshold event len: %d\n'%len(threshold_events))
+#        for ii in xrange(len(threshold_events)):
+#            f.write('Threshold event: %d\n'%threshold_events[ii])
+
+#        f.write('Frame start len: %d\n'%len(frame_starts))
+#        f.write('Frame cnt len: %d\n'%frame_cnt.size)
+#        for ii in xrange(len(frame_starts)):
+#            f.write('Frame start: %d, Frame cnt: %d\n'%(frame_starts[ii] , frame_cnt[ii]))
+
+#        f.write('ENDENDENDEND\n\n')
+        
+#        f.close()
 
 
         #####################################
         # Write data to file
-#        f = open('./collect_%d.bin'%frame_cnt[0], 'wb')
+        beginning = 0
+
+        while 1 :
+            if len(threshold_events) == 0 :
+                ending = ninput_items
+            else :
+                ending = threshold_events[0]
+                threshold_events = threshold_events[1:]
 
 
-        start_idx = 0
+            if beginning != ending :
 
-        while True :
-            if frame_cnt.size == 0 :
-                input[start_idx:ninput_items].tofile(self.f)
+                if self.f == None :
+                    self.f = open('./collect_%d.bin'%self.file_counter, 'wb')
+                    self.file_counter += 1
+
+                start_idx = beginning
+
+                while 1 :
+                    if frame_cnt.size == 0 or frame_starts[0] >= ending :
+                        input[start_idx:ending].tofile(self.f)
+                        break
+
+                    input[start_idx:frame_starts[0]].tofile(self.f)
+
+                    self.FrameConf.START_OF_FRAME.tofile(self.f)
+                    self.FrameConf.DATA_FRAME_ID.tofile(self.f)
+            
+                    frame_cnt[0].newbyteorder('B').tofile(self.f)
+
+
+                    start_idx = frame_starts[0]
+
+                    frame_cnt = frame_cnt[1:]
+                    frame_starts = frame_starts[1:]
+
+
+            beginning = ending
+
+            if beginning >= ninput_items :
                 break
 
+            if self.f is not None:
+                self.f.close()
+                self.f = None
 
-            input[start_idx:frame_starts[0]].tofile(self.f)
-
-            self.FrameConf.START_OF_FRAME.tofile(self.f)
-            self.FrameConf.DATA_FRAME_ID.tofile(self.f)
-            
-            frame_cnt[0].newbyteorder('B').tofile(self.f)
-
-
-            start_idx = frame_starts[0]
-
-            frame_cnt = frame_cnt[1:]
-            frame_starts = frame_starts[1:]
-
-
-#        self.f.flush()                
-#        f.close()
+#        g = open('./work.txt', 'ab')
+#        g.write("FrameToFileSink stop\n")
+#        g.close
 
         return ninput_items