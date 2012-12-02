4dfdea996b28340b0a5fcb33af59dcd788c80606
babjak.b@gmail.com <babjak.b@gmail.com>
2012-12-02 19:42:43 +0000

    - Frame collector fixed

--- tools/ControlRecord/ControlRecord_GNUradio/FFT_test.py
+++ tools/ControlRecord/ControlRecord_GNUradio/FFT_test.py
@@ -70,20 +70,46 @@ class Top_Block(grc_wxgui.top_block_gui):
 		)
 
         self.Add(self.wxgui_fftsink2_0.win)
+
+        self.wxgui_fftsink2_1 = fftsink2.fft_sink_f(
+			self.GetWin(),
+			baseband_freq=0,
+			y_per_div=10,
+			y_divs=10,
+			ref_level=0,
+			ref_scale=2.0,
+			sample_rate=samp_rate,
+			fft_size=512,
+			fft_rate=10*480,
+			average=False,
+			avg_alpha=None,
+			title="FFT Plot",
+			peak_hold=False,
+		)
+
+        self.Add(self.wxgui_fftsink2_1.win)
+
         self.gr_short_to_float_0 = gr.short_to_float(1, 32768)
+        self.gr_short_to_float_1 = gr.short_to_float(1, 32768)
         self.high_pass_filter_0 = gr.fir_filter_fff(1, firdes.high_pass(
 			1, samp_rate, cutoff, transition, firdes.WIN_RECTANGULAR, 6.76))
-        self.gr_null_sink_0 = gr.null_sink(gr.sizeof_short*1)
+        self.high_pass_filter_1 = gr.fir_filter_fff(1, firdes.high_pass(
+			1, samp_rate, cutoff, transition, firdes.WIN_RECTANGULAR, 6.76))
 
 
         ##################################################
         # Connections
         ##################################################
         self.connect((self.frame_source, 0), (self.gr_short_to_float_0, 0))
+        self.connect((self.frame_source, 1), (self.gr_short_to_float_1, 0))
+
         self.connect((self.gr_short_to_float_0, 0), (self.high_pass_filter_0, 0))
-        self.connect((self.frame_source, 1), (self.gr_null_sink_0, 0))
+        self.connect((self.gr_short_to_float_1, 0), (self.high_pass_filter_1, 0))
+
         self.connect((self.high_pass_filter_0, 0), (self.wxgui_fftsink2_0, 0))
         self.connect((self.high_pass_filter_0, 0), (self.wxgui_scopesink2_0, 0))
+
+        self.connect((self.high_pass_filter_1, 0), (self.wxgui_fftsink2_1, 0))
 
 
 ################################################################################
