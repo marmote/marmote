cea895656d73ab3efe2c88309cfa7f4333cfa952
Sandor Szilvasi <sszilvasi@users.noreply.github.com>
2012-12-13 00:11:56 +0000

    Added Simulink model for the various modulations.

---
+++ simulation/simulink/OQPSK/oqpsk_syn_init.m
@@ -0,0 +1,3 @@
+oqpsk_syn_params.N = 16;
+oqpsk_syn_params.rc_group_delay = 4;
+oqpsk_syn_params.snr = 10;
---
+++ simulation/simulink/QPSK/qpsk_ref_init.m
@@ -0,0 +1,3 @@
+qpsk_ref_params.N = 16;
+qpsk_ref_params.rc_group_delay = 4;
+qpsk_ref_params.snr = 10;
