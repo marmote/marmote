517aa8cdc903af09abab76fa37f3a1ca00b2cdc9
babjak.b@gmail.com <babjak.b@gmail.com@1ba9f358-dc1c-8ea8-ff8b-508690f606d3>
2012-12-17 17:30:25 +0000

    - File source component

--- trunk/tools/ControlRecord/ControlRecord_GNUradio/GNUradio_blocks/gr-frame_source/lib/frame_source_ss_impl.cc
+++ trunk/tools/ControlRecord/ControlRecord_GNUradio/GNUradio_blocks/gr-frame_source/lib/frame_source_ss_impl.cc
@@ -70,30 +70,84 @@ frame_source_ss_impl::work(int							noutput_items,
 							gr_vector_const_void_star	&input_items,
 							gr_vector_void_star			&output_items)
 {
+	short 		*out1	=	(short *) 		output_items[0];	
+	short 		*out2	=	(short *) 		output_items[1];	
+
 	int N = noutput_items;
 
-	if (N == 0)
+	if ( N == 0 )
 	{
-		printf("FrameSource error: No space in output buffers!");
+		printf("FrameSource error: No space in output buffers!\n");
 		return 0;
 	}
+    
+	int noutput;
 
-	int noutput = 0;
+	noutput = 0;
+
 
 	while (1)
 	{
-	
-	
+		// 1. Some minor pre-processing steps
+		eNs16b_ret_t ret_s = eNs16b.Process( dfe.byte_buff_len, dfe.frame_starts, dfe.frame_cnt, N - noutput ); 
+
+		if ( ret_s.buff_len )
+		{
+			for (unsigned long i=0; i<N; i++)
+			{
+				out1[i] = *((short*) (&dfe.byte_buff[i*2*2]));
+				out2[i] = *((short*) (&dfe.byte_buff[i*2*2+2]));
+			}
+
+			while ( !ret_s.frame_starts.empty() )
+			{
+				uint64_t offset0 = this->nitems_written(0) + noutput + ret_s.frame_starts.front()/2;
+				uint64_t offset1 = this->nitems_written(1) + noutput + ret_s.frame_starts.front()/2;
+				pmt::pmt_t key		= pmt::pmt_string_to_symbol( "Frame counter" );
+				char temp[50];
+				sprintf( temp, "%d", ret_s.frame_cnt.front() );
+				pmt::pmt_t value	= pmt::pmt_string_to_symbol( temp );
+
+				this->add_item_tag(0, offset0, key, value);
+				this->add_item_tag(1, offset1, key, value);
+
+				ret_s.frame_starts.pop();
+				ret_s.frame_cnt.pop();
+			}
+
+			noutput += ret_s.buff_len/2/2;
+		}
+
+		dfe.ClearFromBeginning( ret_s.buff_len ); // Clear any previous data, that was already processed
+
+		// If we have enough data
+		if ( noutput >= N ) 
+			break;
+
+		// Not enough but there's not gonna be any new
+		if ( s.SourceEmpty() )
+			break;
+
+              
+		// 2. Get some brand new, raw data
+		fs_ret_t ret_s2;
+		ret_s2 = s.GetBuffer(10000);
+            
+
+		// 3. Find data frames (if any) in data
+		unsigned long proc_bytes = dfe.ExtractDataFrames(ret_s2.accum, ret_s2.accum_len);
+		s.ClearFromBeginning(proc_bytes);
 	}
 
 
-	const float *in = (const float *) input_items[0];
-	float *out = (float *) output_items[0];
-
-	// Do <+signal processing+>
-
 	// Tell runtime system how many output items we produced.
-	return noutput_items;
+	if ( noutput == 0 && s.SourceEmpty() )
+	{
+		printf("Source empty.\n");
+		return -1;
+	}
+	else 
+		return noutput;
 }
 
 
