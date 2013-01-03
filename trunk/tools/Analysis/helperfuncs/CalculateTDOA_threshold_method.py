- Analysis functions updated - BUG corrected in threshold component - New AIC based onset calculation method implemented

trunk/tools/Analysis/helperfuncs/CalculateTDOA_threshold_method.py
@@ -1,5 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import Onset_Threshold as OT
 
 def CalculateTDOA_threshold_method(y, y2, T, th_cross_level = 0.15) :
 	AE_start = []
@@ -7,25 +6,8 @@ def CalculateTDOA_threshold_method(y, y2, T, th_cross_level = 0.15) :
 	TD_meas = []
 	
 	for ii in xrange(len(y)) :
-		I_buff = y[ii]
-		Q_buff = y2[ii]
-
-		#Find AE signal beginnings
-		max_val = np.amax(np.absolute(I_buff))
-		val = 0
-		for jj in xrange(I_buff.size) :
-			if I_buff[jj] >= max_val*th_cross_level : 
-				val = jj
-				break
-		AE_start.append(val * T)
-    
-		max_val2 = np.amax(np.absolute(Q_buff))
-		val = 0
-		for jj in xrange(Q_buff.size) :
-			if Q_buff[jj] >= max_val2*th_cross_level : 
-				val = jj
-				break
-		AE_start2.append(val * T)
+		AE_start.append( OT.Onset_Threshold(y[ii], T, th_cross_level ) )
+		AE_start2.append( OT.Onset_Threshold(y2[ii], T, th_cross_level ) )
 		
 		TD_meas.append(AE_start2[-1] - AE_start[-1])
 		