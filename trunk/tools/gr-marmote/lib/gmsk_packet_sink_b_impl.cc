c5ef78266fbe664e83e59fa2c5c82414a71bb14e
sandor.szilvasi@gmail.com <sandor.szilvasi@gmail.com@1ba9f358-dc1c-8ea8-ff8b-508690f606d3>
2013-04-27 23:10:57 +0000

    Added a GMSK message sink block (currently implements frame synchronizer only).

---
+++ trunk/tools/gr-marmote/lib/gmsk_packet_sink_b_impl.cc
@@ -0,0 +1,154 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2013 <+YOU OR YOUR COMPANY+>.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <gr_io_signature.h>
+#include "gmsk_packet_sink_b_impl.h"
+
+#include <iomanip>
+
+
+namespace gr {
+  namespace marmote {
+
+    gmsk_packet_sink_b::sptr
+    gmsk_packet_sink_b::make(bool verbose)
+    {
+      return gnuradio::get_initial_sptr (new gmsk_packet_sink_b_impl(verbose));
+    }
+
+    gmsk_packet_sink_b_impl::gmsk_packet_sink_b_impl(bool verbose)
+      : gr_block("gmsk_packet_sink_b",
+		      gr_make_io_signature(1, 1, sizeof(uint8_t)),
+		      gr_make_io_signature(0, 0, 0)),
+        d_state(STATE_SYNC_SEARCH),
+        d_sync_vector(0x70EED2uL),
+        d_shift_reg(0x0uL),
+        d_verbose(verbose)
+    {
+      set_history(d_sync_vector_len);
+    }
+
+    gmsk_packet_sink_b_impl::~gmsk_packet_sink_b_impl()
+    {
+    }
+
+    void gmsk_packet_sink_b_impl::enter_search(void)
+    {
+        if (d_verbose)
+            std::cout << "@ enter_search" << std::endl;
+
+        d_state = STATE_SYNC_SEARCH;
+        d_shift_reg = 0;
+    }
+
+    void gmsk_packet_sink_b_impl::enter_have_sync(void)
+    {
+        if (d_verbose)
+            std::cout << "@ enter_have_sync" << std::endl;
+
+        d_state = STATE_HAVE_SYNC;
+    }
+
+    int
+    gmsk_packet_sink_b_impl::general_work (int noutput_items,
+                       gr_vector_int &ninput_items,
+                       gr_vector_const_void_star &input_items,
+                       gr_vector_void_star &output_items)
+    {
+        const uint8_t *in = (const uint8_t*) input_items[0];
+        int ninput = ninput_items[0];
+        int nprocd = 0; // number of processed input samples
+
+        while ( nprocd < ninput - d_sync_vector_len )
+        {
+            // if (d_verbose)
+            //   std::cout << ">>> Entering state machine" << std::endl;
+
+            switch(d_state) {
+
+              case STATE_SYNC_SEARCH:
+                if (d_verbose)
+                    std::cout << ">>> SYNC SEARCH: ninput=" << ninput << " d_sync_vector=" << d_sync_vector << std::endl;
+
+                while (nprocd < ninput - d_sync_vector_len)
+                {
+                    std::cout << (unsigned int)in[nprocd] << " "; // << std::endl;
+
+                    d_shift_reg <<= 1;
+                    d_shift_reg &= 0x00FFFFFF;
+
+                    if ( in[nprocd++] == 0 ) // FIXME: should be '!='
+                        d_shift_reg |= 0x1uL;
+
+                    if (d_verbose)
+                        std::cout << nprocd << ": " << std::setfill('0') << std::setw(6) << std::hex
+                            << d_shift_reg << " ^ " << d_sync_vector << " = " << std::setw(6)
+                            << (d_shift_reg ^ d_sync_vector) << std::dec
+                            << " (" << nprocd << "/" << ninput << ")" << std::endl;
+
+                    if ( (d_shift_reg ^ d_sync_vector) == 0 )
+                    {
+                        enter_have_sync();
+                        break; // while
+                    }
+                }
+                break;
+
+              case STATE_HAVE_SYNC:
+                if (d_verbose)
+                {
+                    std::cout << ">>> Entering HAVE SYNC" << std::endl;
+                    std::cout << " (" << nprocd << "/" << ninput << ")" << std::endl;
+
+                    // while (nprocd < ninput - d_sync_vector_len)
+                    // {
+                    //     nprocd++;
+                    // }
+                    enter_search(); // FIXME: restart for now
+                }
+
+
+                break;
+
+              case STATE_HAVE_HEADER:
+                if (d_verbose)
+                    std::cout << ">>> Entering HAVE HEADER" << std::endl;
+                break;
+
+              default:
+                if (d_verbose)
+                std::cout << ">>> Invalid state" << std::endl;
+                break;
+            }
+        }
+
+
+        consume_each (nprocd);
+
+        return 0;
+    }
+
+  } /* namespace marmote */
+} /* namespace gr */
+
