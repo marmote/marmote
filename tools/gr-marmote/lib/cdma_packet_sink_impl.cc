cac6494540f8b3688773cbfc0131c35c83710d7e
Sandor Szilvasi <sszilvasi@users.noreply.github.com>
2013-06-26 23:50:54 +0000

    Revision used for first CDMA measurements.

--- tools/gr-marmote/lib/cdma_packet_sink_impl.cc
+++ tools/gr-marmote/lib/cdma_packet_sink_impl.cc
@@ -28,114 +28,149 @@
 #include <iomanip>
 
 namespace gr {
-  namespace marmote {
+	namespace marmote {
 
-    cdma_packet_sink::sptr
-    cdma_packet_sink::make(bool debug, int id)
-    {
-      return gnuradio::get_initial_sptr (new cdma_packet_sink_impl(debug, id));
-    }
+		cdma_packet_sink::sptr cdma_packet_sink::make(bool debug, int id)
+		{
+			return gnuradio::get_initial_sptr (new cdma_packet_sink_impl(debug, id));
+		}
 
-    cdma_packet_sink_impl::cdma_packet_sink_impl(bool debug, int id)
-      : gr_block("cdma_packet_sink",
-		      gr_make_io_signature(0, 0, 0),
-		      gr_make_io_signature(0, 0, 0)),
-        d_debug(debug),
-        d_id(id),
-        d_polynomial(0x1021)
-    {
-      message_port_register_in(pmt::mp("in"));
-      set_msg_handler(pmt::mp("in"), boost::bind(&cdma_packet_sink_impl::process_packet, this, _1));
-    }
+		cdma_packet_sink_impl::cdma_packet_sink_impl(bool debug, int id)
+			: gr_block("cdma_packet_sink",
+					gr_make_io_signature(0, 0, 0),
+					gr_make_io_signature(0, 0, 0)),
+			d_debug(debug),
+			d_id(id),
+			d_polynomial(0x1021),
+            d_first_pkt_found(false),
+            d_start_seq(0),
+            d_valid_pkt_ctr(0),
+            d_total_pkt(1000) // FIXME
+		{
+			message_port_register_in(pmt::mp("in"));
+			set_msg_handler(pmt::mp("in"), boost::bind(&cdma_packet_sink_impl::process_packet, this, _1));
+		}
 
-    cdma_packet_sink_impl::~cdma_packet_sink_impl()
-    {
-    }
+		cdma_packet_sink_impl::~cdma_packet_sink_impl()
+		{
+		}
 
-    void cdma_packet_sink_impl::process_packet(pmt::pmt_t pkt)
-    {
-      if (pmt::pmt_is_blob(pkt))
-      {
-        // std::cout << "Processing packet..." << " [" << (int)(pmt::pmt_blob_length(pkt)) << " bits]" << std::endl;
+		void cdma_packet_sink_impl::process_packet(pmt::pmt_t pkt)
+		{
+			if (pmt::pmt_is_blob(pkt))
+			{
+				// std::cout << "Processing packet..." << " [" << (int)(pmt::pmt_blob_length(pkt)) << " bits]" << std::endl;
 
-        if (d_debug)
-        {
-          std::cout << "    #" << d_id << " <- ";
+				if (d_debug)
+				{
+					std::cout << "    #" << d_id << " <- ";
 
-          int k = 0;
-          uint8_t octet = 0;
-          uint8_t* payload = (uint8_t*)pmt::pmt_blob_data(pkt);
-          uint16_t crc;
+					int k = 0;
+					uint8_t octet = 0;
+					uint8_t* payload = (uint8_t*)pmt::pmt_blob_data(pkt);
+					uint16_t crc;
 
-          for (int i = 0; i < pmt::pmt_blob_length(pkt); i++)
-          {
-            octet = (octet << 1) | payload[i];
-            if (i % 8 == 7)
-            {
-              d_buf[k++] = octet;
-              std::cout << std::setw(2) << std::setfill('0') << std::hex << (int)octet << std::dec << " ";
-            }
-          }
+                    // Assemble packet
+					for (int i = 0; i < pmt::pmt_blob_length(pkt); i++)
+					{
+						octet = (octet << 1) | payload[i];
+						if (i % 8 == 7)
+						{
+							d_buf[k++] = octet;
+							std::cout << std::setw(2) << std::setfill('0') << std::hex << (int)octet << std::dec << " ";
+						}
+					}
 
-          if (k >= 3 && (crc_16(d_buf, k-2) != ((d_buf[k-2] << 8) + d_buf[k-1])))
-          {
-            std::cout << "#";
-          }
-          std::cout << std::endl;
+                    // Check CRC
+					if (k >= 3 && (crc_16(d_buf, k-2) != ((d_buf[k-2] << 8) + d_buf[k-1])))
+					{
+						std::cout << "#" << std::endl;
+                        return;
+					}
 
+					// std::cout << std::endl;
 
-          // for (int i = 0; i < pmt::pmt_blob_length(pkt); i++)
-          // {
-          //   std::cout << std::setw(2) << (int)payload[i] << " ";
-          // }
-          // std::cout << std::endl;
-        }
-      }
-      else if (pmt::pmt_is_symbol(pkt))
-      {
+                    // Process valid packet
+                    uint16_t seq;
+                    seq = (d_buf[1] & 0xFF) << 8;
+                    seq += d_buf[2] & 0xFF;
 
-        const char* data = pmt::pmt_symbol_to_string(pkt).data();
+                    if (d_first_pkt_found == false)
+                    {
+                        d_start_seq = seq;
+                        d_first_pkt_found = true;
+                        d_valid_pkt_ctr = 1;
+                        // std::cout << "SEQ: " << (int)seq << " <" << std::endl;
+                        std::cout << "<" << std::endl;
+                    }
+                    else
+                    {
+                        // std::cout << "SEQ: " << (int)seq;
+                        if (seq - d_start_seq > d_total_pkt - 1)
+                        {
+                            std::cout << "<" << std::endl;
+                            // std::cout << "start_seq: " << d_start_seq << " current_seq: " << seq << " diff_seq: " << (seq - d_start_seq) << std::endl;
+                            // std::cout << "valid_pkt: " << d_valid_pkt_ctr << " / total_pkt: " << d_total_pkt <<  " = ";
+                            // std::cout << (float)d_valid_pkt_ctr/(float)d_total_pkt << std::endl;
 
-        for (int j = 0; j < pmt::pmt_symbol_to_string(pkt).length(); j++)
-        {
-          std::cout << std::setw(2) << (int)data[j] << " ";
-        }
-        std::cout << std::endl;
-      }
-      else
-      {
-        std::cerr << "@cdma_packet_sink_impl::process_packet: unexpected PMT type" << std::endl;
-        assert(false);
-      }
-    }
+                            std::cout << "#" << d_id << " => PER: " << 1.0 - (float)d_valid_pkt_ctr/(float)d_total_pkt;
+                            std::cout << " (" << d_total_pkt-d_valid_pkt_ctr << "/" << d_total_pkt << ")" << std::endl;
 
-    uint16_t cdma_packet_sink_impl::crc_16(const uint8_t data[], uint8_t length)
-    {
-      uint8_t bit;
-      uint8_t byte;
-      uint16_t crc;
+                            while(1);
+                        }
 
-      crc = 0;
-      for (byte = 0; byte < length; ++byte)
-      {
-        crc ^= (data[byte] << 8);
+                        std::cout << "(" << d_valid_pkt_ctr << " / " << (seq - d_start_seq) << " / " << d_total_pkt <<  ")";
+                        std::cout << std::endl;
+                        d_valid_pkt_ctr++;
+                    }
+                    // std::cout << "SEQ: " << (int)seq << std::endl;
+				}
+			}
+			else if (pmt::pmt_is_symbol(pkt))
+			{
 
-        for (bit = 8; bit > 0; --bit)
-        {
-          if (crc & (1 << 15))
-          {
-            crc = (crc << 1) ^ d_polynomial;
-          }
-          else
-          {
-            crc = (crc << 1);
-          }
-        }
-      }
+				const char* data = pmt::pmt_symbol_to_string(pkt).data();
 
-      return crc;
-    }
+				for (int j = 0; j < pmt::pmt_symbol_to_string(pkt).length(); j++)
+				{
+					std::cout << std::setw(2) << (int)data[j] << " ";
+				}
+				std::cout << std::endl;
+			}
+			else
+			{
+				std::cerr << "@cdma_packet_sink_impl::process_packet: unexpected PMT type" << std::endl;
+				assert(false);
+			}
+		}
 
-  } /* namespace marmote */
+		uint16_t cdma_packet_sink_impl::crc_16(const uint8_t data[], uint8_t length)
+		{
+			uint8_t bit;
+			uint8_t byte;
+			uint16_t crc;
+
+			crc = 0;
+			for (byte = 0; byte < length; ++byte)
+			{
+				crc ^= (data[byte] << 8);
+
+				for (bit = 8; bit > 0; --bit)
+				{
+					if (crc & (1 << 15))
+					{
+						crc = (crc << 1) ^ d_polynomial;
+					}
+					else
+					{
+						crc = (crc << 1);
+					}
+				}
+			}
+
+			return crc;
+		}
+
+	} /* namespace marmote */
 } /* namespace gr */
 
