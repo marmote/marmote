c8413a8cb72d6fb2590d2ae0fd2bb678a48b44ca
sandor.szilvasi@gmail.com <sandor.szilvasi@gmail.com@1ba9f358-dc1c-8ea8-ff8b-508690f606d3>
2013-10-01 19:55:14 +0000

    Fixed some bugs in mm_rx_cfile.sh

--- trunk/tools/gr-marmote/meas/mm_rx_cfile.sh
+++ trunk/tools/gr-marmote/meas/mm_rx_cfile.sh
@@ -17,7 +17,8 @@ done
 FILENAME=$(printf "f%s_%03d.dat" $1 $SEQ)
 echo Recording to file $FILENAME...
 
-uhd_rx_cfile -a addr=192.168.10.2 -f "$1"M --samp-rate=25M -g 15 -N 55M $1
+#uhd_rx_cfile -a addr=192.168.10.2 -f "$1"M --samp-rate=25M -g 20 -N 55M $FILENAME
+uhd_rx_cfile -f "$1"M --samp-rate=25M -g 20 -N 55M $FILENAME
 
 if [ $? -ne 0 ]; then
     echo Failure
