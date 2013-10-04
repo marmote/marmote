6f74cdfe6a04fb4dfd17b3b8a45d2cd43b9e0f53
babjak.b@gmail.com <babjak.b@gmail.com@1ba9f358-dc1c-8ea8-ff8b-508690f606d3>
2013-10-04 17:58:57 +0000

    - Improved wavelet analysis

--- trunk/tools/Analysis/helperfuncs/FullWPD.py
+++ trunk/tools/Analysis/helperfuncs/FullWPD.py
@@ -2,7 +2,7 @@ import numpy as np
 import pywt
 
 
-def WDP_rec(c, N=None, flip = False, wavelet='haar') :
+def WDP_rec(c, N=None, flip = False, wavelet='haar', E=None) :
 #init
     PacketTable = []
     PacketTableLevel = []
@@ -28,10 +28,20 @@ def WDP_rec(c, N=None, flip = False, wavelet='haar') :
         cHigh, cLow = pywt.dwt(c, w)
     else :
         cLow, cHigh = pywt.dwt(c, w)
-        
+
+    El = np.sum(cLow**2)
+    Eh = np.sum(cHigh**2)
+    if E is None:
+        E = np.sum(c**2)
+
+    scaling_coef = np.sqrt( E/(El+Eh) )
+
+    cLow *= scaling_coef
+    cHigh *= scaling_coef
+
 #recursive levels
-    PacketTable_Low = WDP_rec(cLow, N=N/2, wavelet=wavelet)
-    PacketTable_High = WDP_rec(cHigh, N=N/2, flip = True, wavelet=wavelet)
+    PacketTable_Low = WDP_rec( cLow, N=N/2, wavelet=wavelet, E=El*(scaling_coef**2) )
+    PacketTable_High = WDP_rec( cHigh, N=N/2, flip = True, wavelet=wavelet, E=Eh*(scaling_coef**2) )
     
     for ii in xrange(len(PacketTable_Low)) :
         PacketTable.append(PacketTable_Low[ii] + PacketTable_High[ii])
