cc7a0c481339f5a8ec83e9f0f21b325d67ce739f
babjak.b@gmail.com <babjak.b@gmail.com>
2013-12-12 18:38:23 +0000

    Normal distribution on 2D histogram

---
+++ tools/Analysis/helperfuncs/Estimate_Multivariate_Normal_Parameters.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def Estimate_Multivariate_Normal_Parameters(x):
+    mu = np.mean(x, 1)
+    sigma = np.cov(x)
+
+    return mu, sigma
--- tools/Analysis/helperfuncs/GMM_estimate_EM.py
+++ tools/Analysis/helperfuncs/GMM_estimate_EM.py
@@ -35,7 +35,7 @@ def likelihood(x, alpha, mu, sigma):
 	for n in xrange(N):
 		temp = 0.0
 		for g in xrange(G):
-			temp += alpha[g] * MNPDF.MultivariateNormalPDF(x[n,:], mu[g,:], sigma[:,:,g])
+			temp += alpha[g] * MNPDF.MultivariateNormalPDF(x[n,:], mu[g,:], sigma[g,:,:])
 			if temp == 0.0:
 				temp = small_float
 		l += np.log(temp)
@@ -93,7 +93,7 @@ def GMM_estimate_EM(x, alpha = None, mu = None, sigma = None, iterations = 10) :
 	alpha /= np.sum(alpha)
 
 	N = x.shape[0]
-	D = sigma.shape[0]
+	D = sigma.shape[1]
 	G = alpha.size
 
 	L = likelihood(x, alpha, mu, sigma)
@@ -108,9 +108,9 @@ def GMM_estimate_EM(x, alpha = None, mu = None, sigma = None, iterations = 10) :
 		for g in xrange(G):
 			denom = 0.0
 			for j in xrange(G):
-				denom += alpha[j] * MNPDF.MultivariateNormalPDF( x[n,:], mu[j,:], sigma[:,:,j] )
+				denom += alpha[j] * MNPDF.MultivariateNormalPDF( x[n,:], mu[j,:], sigma[j,:,:] )
 
-			r[n, g] = alpha[g] * MNPDF.MultivariateNormalPDF( x[n,:], mu[g,:], sigma[:,:,g] ) / denom
+			r[n, g] = alpha[g] * MNPDF.MultivariateNormalPDF( x[n,:], mu[g,:], sigma[g,:,:] ) / denom
 
 	Nk = np.zeros(G)
 	for g in xrange(G):
@@ -120,7 +120,7 @@ def GMM_estimate_EM(x, alpha = None, mu = None, sigma = None, iterations = 10) :
 
 	alpha_new = np.zeros(G)
 	mu_new = np.zeros((G,D))
-	sigma_new = np.zeros((D,D,G))
+	sigma_new = np.zeros((G,D,D))
 	for g in xrange(G):
 		alpha_new[g] = Nk[g] / N
 
@@ -130,8 +130,8 @@ def GMM_estimate_EM(x, alpha = None, mu = None, sigma = None, iterations = 10) :
 
 
 		for n in xrange(N):
-			sigma_new[:,:,g] += r[n,g] * np.outer( x[n,:] - mu_new[g,:],  x[n,:] - mu_new[g,:] )
-		sigma_new[:,:,g] /= Nk[g]
+			sigma_new[g,:,:] += r[n,g] * np.outer( x[n,:] - mu_new[g,:],  x[n,:] - mu_new[g,:] )
+		sigma_new[g,:,:] /= Nk[g]
 #		sigma_new[k] = np.sqrt(sigma_new[k])
 
 #####
@@ -161,7 +161,7 @@ def GMM_estimate_EM(x, alpha = None, mu = None, sigma = None, iterations = 10) :
 if __name__ == "__main__":
 	alpha0 = np.array([0.5, 0.5, 0.5])
 	mu0 = np.array([[10.0, 10.0], [100.0, 100.0], [50.0, 50.0]])
-	sigma0 = np.array( [[[1.0, 8.0, 1.0],[0.0, 0.0, 0.0]], [[0.0, 0.0, 0.0],[1.0, 8.0, 0.3]]] )
+	sigma0 = np.array( [[[1.0, 0.0],[0.0, 1.0]], [[8.0, 0.0],[0.0, 8.0]], [[1.0, 0.0],[0.0, 0.3]]] )
 
 	x = np.array([[10.0, 10.4], [10.2, 10.2], [10.3, 10.4], [50.4, 50.9], [103, 97], [110, 100], [96, 90], [49, 54]])
 
